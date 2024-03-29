1f830379cd6740a7c9adf229b56db91cdef45692
danielmorgan779 <danielm462005@gmail.com>
2024-03-29 10:54:27 -0500

    movement
    we did it

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -9,6 +9,7 @@ public class PlayerMotor : MonoBehaviour
     private bool isGrounded;
     public float speed = 5f;
     public float gravity = -9.8f;
+    public float jumpHeight = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,5 +33,12 @@ public class PlayerMotor : MonoBehaviour
             playerVelocity.y = -2f;
         controller.Move(playerVelocity * Time.deltaTime);
         Debug.Log(playerVelocity.y);
+    }
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
     }
 }
