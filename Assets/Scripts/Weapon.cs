041487978b9e8b1cf3a51146a495ec41f9960bb8
danielmorgan779 <danielm462005@gmail.com>
2024-04-11 03:05:29 -0500

    bullet cs
    script

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30;
+    public float bulletPrefabLifeTime = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Left mouse click
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        throw new NotImplementedException(); 
+    }
+}
